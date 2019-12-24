16d22d91fcbae8937fd1b7efa569b79ef984dcba
johnqdev2019 <diamondfloor56>
2019-12-23 18:13:05 -0600

    Initial code

---
+++ Intro_to_classes.py
@@ -0,0 +1,23 @@
+# Python Object Oriented Programming
+
+class Employee:
+    pass
+
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+print(emp_1)
+print(emp_2)
+
+emp_1.first = 'Seth'
+emp_1.last = 'Bergman'
+emp_1.email = 'seth.bergman@gmail.com'
+emp_1.pay = 70000
+
+emp_2.first = 'Test'
+emp_2.last = 'Employee'
+emp_2.email = 'test.employee@gmail.com'
+emp_2.pay = 80000
+
+print(emp_1)
