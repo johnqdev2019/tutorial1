badd0d8580ff9e014d6b05a311bc4ddfe5f3fa17
johnqdev2019 <diamondfloor56>
2019-12-23 18:21:54 -0600

    Removed manual user input code

--- Intro_to_classes.py
+++ Intro_to_classes.py
@@ -1,5 +1,6 @@
 # Python Object Oriented Programming
 
+
 class Employee:
     pass
 
@@ -10,14 +11,15 @@ emp_2 = Employee()
 print(emp_1)
 print(emp_2)
 
-emp_1.first = 'Seth'
-emp_1.last = 'Bergman'
-emp_1.email = 'seth.bergman@gmail.com'
-emp_1.pay = 70000
+# emp_1.first = 'Seth'
+# emp_1.last = 'Bergman'
+# emp_1.email = 'seth.bergman@gmail.com'
+# emp_1.pay = 70000
+#
+# emp_2.first = 'Test'
+# emp_2.last = 'Employee'
+# emp_2.email = 'test.employee@gmail.com'
+# emp_2.pay = 80000
 
-emp_2.first = 'Test'
-emp_2.last = 'Employee'
-emp_2.email = 'test.employee@gmail.com'
-emp_2.pay = 80000
-
-print(emp_1)
+print(emp_1.email)
+print(emp_2.email)
