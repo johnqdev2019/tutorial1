6eefe323bc94fdb57dd8ac94b0f4cf04ad5b2bc8
johnqdev2019 <diamondfloor56>
2019-12-27 12:36:30 -0600

    Added date

--- Intro_to_classes.py
+++ Intro_to_classes.py
@@ -1,4 +1,5 @@
 # Python Object Oriented Programming
+# Date: 12/27/19
 
 
 class Employee:
@@ -7,25 +8,14 @@ class Employee:
         self.first = first
         self.last = last
         self.pay = pay
-        self.email = first + '.' + last + '@gmail.com'
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
 
 
 emp_1 = Employee('Seth', 'Bergman', 70000)
 emp_2 = Employee('Test', 'Employee', 60000)
 
-print(emp_1)
-
-# print(emp_1)
-# print(emp_2)
-
-# emp_1.first = 'Seth'
-# emp_1.last = 'Bergman'
-# emp_1.email = 'seth.bergman@gmail.com'
-# emp_1.pay = 70000
-#
-# emp_2.first = 'Test'
-# emp_2.last = 'Employee'
-# emp_2.email = 'test.employee@gmail.com'
-# emp_2.pay = 80000
-
 print(emp_1.email)
+print(emp_1.fullname())
